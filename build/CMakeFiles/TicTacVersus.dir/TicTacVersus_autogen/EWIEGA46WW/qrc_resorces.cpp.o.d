43bf34063c3af98394f2d6ec02f5545f3762de41
bledniyyy <97696388+bledniyyy@users.noreply.github.com>
2025-09-27 14:05:45 +0300

    и это не надо

--- build/CMakeFiles/TicTacVersus.dir/TicTacVersus_autogen/EWIEGA46WW/qrc_resorces.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TicTacVersus.dir/TicTacVersus_autogen/EWIEGA46WW/qrc_resorces.cpp.o: \
- /home/b/Рабочий\ стол/TicTakVersus/build/TicTacVersus_autogen/EWIEGA46WW/qrc_resorces.cpp \
- /usr/include/stdc-predef.h
